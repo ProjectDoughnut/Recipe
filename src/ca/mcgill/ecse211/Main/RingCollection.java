59d811e8eea9fca10edec87998f21e9bbad2cb91
Binyuan Sun <sunbinyuan01@hotmail.com>
2018-11-20 00:24:53 -0500

    added ring collection class (WIP)

--- src/ca/mcgill/ecse211/Main/RingCollection.java
+++ src/ca/mcgill/ecse211/Main/RingCollection.java
@@ -21,123 +21,98 @@ public class RingCollection extends Thread{
 	private Odometer odo;
 	private Navigation nav;
 	private EV3MediumRegulatedMotor servo;
+	private float[] origin;
 	private float[][] tunnel;
 	private float[][] island;
 	private float[] tree;
 	private double WHEEL_RADIUS;
-	private double sensorDistance;
 	private double toBranch = 10;
 	public static Object done = new Object();
 	
 	public boolean running;
 	
-	public RingCollection(Odometer odo, Navigation nav, EV3MediumRegulatedMotor servo, float[][] tunnel, float[] tree, float[][] island, double WHEEL_RADIUS, double sensorDistance) {
+	public RingCollection(Odometer odo, Navigation nav, EV3MediumRegulatedMotor servo, float[] origin, float[][] tunnel, float[] tree, float[][] island, double WHEEL_RADIUS) {
 		this.odo = odo;
 		this.nav = nav;
 		this.servo = servo;
+		this.origin = origin;
 		this.tree = tree;
 		this.tunnel = tunnel;
 		this.island = island;
 		this.WHEEL_RADIUS = WHEEL_RADIUS;
-		this.sensorDistance = sensorDistance;
 		this.running = true;
 	}
 	
-	@SuppressWarnings("static-access")
 	public void run() {
 		
-
 		//Lock the color thread until this thread begins
 
-		
 		/*
 		 * Check that the robot is done navigating to tree first?????
 		 * Then, determine which orientation it is in
 		 */
-		
-		if (nav.tunnelPointingX && nav.tunnelUp) {
-			
-			//This is not taking into account island or wall parameters
-			//SHOULD THESE BE SYNCTRAVELTO????
-			getRings();
-			nav.syncTravelTo(tree[0] - 1, tree[1] - 1);
-			nav.syncTravelTo(tree[0] - 1, tree[1]);
-			nav.turnTo(odo.getXYT()[2], 90);
-			getRings();
-			nav.syncTravelTo(tree[0] - 1, tree[1] + 1);
-			nav.syncTravelTo(tree[0], tree[1] + 1);
-			//Not sure about this turn
-			nav.turnTo(odo.getXYT()[2], 180);
-			getRings();
-			nav.syncTravelTo(tree[0] + 1, tree[1] + 1);
-			nav.syncTravelTo(tree[0] + 1, tree[1]);
-			nav.turnTo(odo.getXYT()[2], -90);
-			getRings();
-			nav.syncTravelTo(tree[0] + 1, tree[1] - 1);
-			nav.syncTravelTo(tree[0], tree[1] - 1);
-			
-		}
-		else if (nav.tunnelPointingX && !nav.tunnelUp) {
-			
-			getRings();
-			nav.syncTravelTo(tree[0] + 1, tree[1] + 1);
-			nav.syncTravelTo(tree[0] + 1, tree[1]);
-			//Not sure about this angle:
-			nav.turnTo(odo.getXYT()[2], -90);
-			getRings();
-			nav.syncTravelTo(tree[0] + 1, tree[1] - 1);
-			nav.syncTravelTo(tree[0], tree[1] - 1);
-			//Not sure about this angle:
-			nav.turnTo(odo.getXYT()[2], 0);
-			getRings();
-			nav.syncTravelTo(tree[0] - 1, tree[1] - 1);
-			nav.syncTravelTo(tree[0] - 1, tree[1]);
-			//Not sure:
-			nav.turnTo(odo.getXYT()[2], 90);
-			getRings();
-			nav.syncTravelTo(tree[0] - 1, tree[1] + 1);
-			nav.syncTravelTo(tree[0], tree[1] + 1);
-			
-		}
-		else if (!nav.tunnelPointingX && nav.tunnelToLeft) {
-			
-			getRings();
-			nav.syncTravelTo(tree[0] + 1, tree[1] - 1);
-			nav.syncTravelTo(tree[0], tree[1] - 1);
-			nav.turnTo(odo.getXYT()[2], 0);
-			getRings();
-			nav.syncTravelTo(tree[0] - 1, tree[1] - 1);
-			nav.syncTravelTo(tree[0] - 1, tree[1]);
-			//Not sure about this turn
-			nav.turnTo(odo.getXYT()[2], 90);
-			getRings();
-			nav.syncTravelTo(tree[0] - 1, tree[1] + 1);
-			nav.syncTravelTo(tree[0], tree[1] + 1);
-			nav.turnTo(odo.getXYT()[2], 180);
-			getRings();
-			nav.syncTravelTo(tree[0] + 1, tree[1] + 1);
-			nav.syncTravelTo(tree[0] + 1, tree[1]);
-			
-		}
-		else if (!nav.tunnelPointingX && !nav.tunnelToLeft) {
-			
-			getRings();
-			nav.syncTravelTo(tree[0] - 1, tree[1] + 1);
-			nav.syncTravelTo(tree[0], tree[1] + 1);
-			nav.turnTo(odo.getXYT()[2], 180);
-			getRings();
-			nav.syncTravelTo(tree[0] + 1, tree[1] + 1);
-			nav.syncTravelTo(tree[0] + 1, tree[1]);
-			//Not sure about this turn
-			nav.turnTo(odo.getXYT()[2], -90);
-			getRings();
-			nav.syncTravelTo(tree[0] + 1, tree[1] - 1);
-			nav.syncTravelTo(tree[0], tree[1] - 1);
-			nav.turnTo(odo.getXYT()[2], 0);
-			getRings();
-			nav.syncTravelTo(tree[0] - 1, tree[1] - 1);
-			nav.syncTravelTo(tree[0] - 1, tree[1]);
-		}
+  		int yOnRight = 0;
+  		int xOnRight = 0;
+  		float[] tunnelVector = new float []{tunnel[1][0] -tunnel[0][0],tunnel[1][1] -tunnel[0][1]};
+  		
+  		if (tunnelVector[0] > 0 && tunnelVector[1] > 0  || tunnelVector[0] < 0 && tunnelVector[1] < 0) {
+  		// y tunnel
+  			if (tree[0] > (tunnel[0][0] + tunnel[1][0])/2 ) {
+  				yOnRight = -1;
+  			} else {
+  				yOnRight = 1;
+  			}
+  		} else if (tunnelVector[0] > 0 && tunnelVector[1] < 0 || tunnelVector[0] < 0 && tunnelVector[1] > 0) {
+  			// x tunnel
+  			if (tree[1] > (tunnel[0][1] + tunnel[1][1])/2 ) {
+  				xOnRight = 1;
+  			} else {
+  				xOnRight = -1;
+  			}
+  		}
+  		
+  		for (int i = 0; i< 4; i++) {
+  			// four sides of the square
+  			//    -----x-----
+  			//    |    |    |
+  			//    x----O----x
+  			//    |    |    |
+  			//    -----x-----
+  			
+  			getRings();
+  			// note that since only xOnRight or yOnRight
+  			// one of the tmp values will actually have a change
+  			float tmpX = origin[0] + xOnRight;
+  			float tmpY = origin[1] + yOnRight;
+  			nav.syncTravelTo(tmpX, tmpY);
+  			// try to understand logic here
+  			// basically, if x is some value, y will because the negative of x
+  			// if y is some value, x will become y
+  			int tmpXOnRight = xOnRight;
+  			xOnRight = yOnRight;
+  			yOnRight = -tmpXOnRight;
+
+  			nav.syncTravelTo(tmpX-xOnRight, tmpY-yOnRight);
+  			
+  			// i skipped an intermediate step here
+  			xOnRight = -xOnRight;
+			yOnRight = -yOnRight;  			
+  			
+  			float angle = 0;
+  			if (xOnRight == 1) {
+  				angle = 0;
+  			} else if (xOnRight == -1) {
+  				angle = 180;
+  			} else if (yOnRight == 1) {
+  				angle = 270;
+  			} else if (yOnRight == -1) {
+  				angle = 90;
+  			}
+  			
+  			nav.turnTo(odo.getXYT()[2], angle);
+  			
+  		}
+
 	}
 
 	/*
