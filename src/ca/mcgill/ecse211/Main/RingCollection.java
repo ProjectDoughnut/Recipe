70250488c5d8408bca02327c8ebfe2c36c9db307
Binyuan Sun <sunbinyuan01@hotmail.com>
2018-11-23 19:33:46 -0500

    finished ring collection algorithm

--- src/ca/mcgill/ecse211/Main/RingCollection.java
+++ src/ca/mcgill/ecse211/Main/RingCollection.java
@@ -4,6 +4,7 @@ import ca.mcgill.ecse211.Color.ColorClassifier;
 import ca.mcgill.ecse211.Light.LightLocalizer;
 import ca.mcgill.ecse211.Odometer.Odometer;
 import ca.mcgill.ecse211.Ultrasonic.USLocalizer;
+import lejos.hardware.Sound;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 
 /**
@@ -26,7 +27,7 @@ public class RingCollection extends Thread{
 	private float[][] island;
 	private float[] tree;
 	private double WHEEL_RADIUS;
-	private double toBranch = 10;
+	private double toBranch = 7.0;
 	public static Object done = new Object();
 	
 	public boolean running;
@@ -55,6 +56,7 @@ public class RingCollection extends Thread{
   		int xOnRight = 0;
   		float[] tunnelVector = new float []{tunnel[1][0] -tunnel[0][0],tunnel[1][1] -tunnel[0][1]};
   		
+  		
   		if (tunnelVector[0] > 0 && tunnelVector[1] > 0  || tunnelVector[0] < 0 && tunnelVector[1] < 0) {
   		// y tunnel
   			if (tree[0] > (tunnel[0][0] + tunnel[1][0])/2 ) {
@@ -71,6 +73,9 @@ public class RingCollection extends Thread{
   			}
   		}
   		
+  		float[][] coordinates = new float[9][];
+  		float[] angles = new float[4];
+  		coordinates[0] = origin.clone();
   		for (int i = 0; i< 4; i++) {
   			// four sides of the square
   			//    -----x-----
@@ -79,25 +84,8 @@ public class RingCollection extends Thread{
   			//    |    |    |
   			//    -----x-----
   			
-  			getRings();
-  			// note that since only xOnRight or yOnRight
-  			// one of the tmp values will actually have a change
-  			float tmpX = origin[0] + xOnRight;
-  			float tmpY = origin[1] + yOnRight;
-  			nav.syncTravelTo(tmpX, tmpY);
-  			// try to understand logic here
-  			// basically, if x is some value, y will because the negative of x
-  			// if y is some value, x will become y
-  			int tmpXOnRight = xOnRight;
-  			xOnRight = yOnRight;
-  			yOnRight = -tmpXOnRight;
-
-  			nav.syncTravelTo(tmpX-xOnRight, tmpY-yOnRight);
   			
-  			// i skipped an intermediate step here
-  			xOnRight = -xOnRight;
-			yOnRight = -yOnRight;  			
-  			
+  			// turn to the right heading
   			float angle = 0;
   			if (xOnRight == 1) {
   				angle = 0;
@@ -109,10 +97,85 @@ public class RingCollection extends Thread{
   				angle = 90;
   			}
   			
-  			nav.turnTo(odo.getXYT()[2], angle);
+  			angles[i] = angle;
   			
+//  			getRings();
+  			// note that since only xOnRight or yOnRight
+  			// one of the tmp values will actually have a change
+  			float tmpX = origin[0] + xOnRight;
+  			float tmpY = origin[1] + yOnRight;
+  			coordinates[2*i+1] = new float[] {tmpX, tmpY};
+  			// try to understand logic here
+  			// basically, if x is some value, y will because the negative of x
+  			// if y is some value, x will become y
+  			int tmpXOnRight = xOnRight;
+  			xOnRight = yOnRight;
+  			yOnRight = -tmpXOnRight;
+
+  			coordinates[2*i+2] = new float[] {tmpX-xOnRight, tmpY-yOnRight};
+  			origin = new float[]{tmpX-xOnRight, tmpY-yOnRight};
+  			// i skipped an intermediate step here
+  			xOnRight = -xOnRight;
+			yOnRight = -yOnRight;
+  		}
+  		
+  		
+  		// figure which points are inside the island/wall
+  		
+  		for (int i = 0; i < 8; i++) {
+  			if (!isIn(island, coordinates[i])) {
+  				coordinates[i] = null;
+  			}
+  		}
+  		
+  		// go in clockwise direction until encountering the point
+  		// that can get the maximum of places by only going counter clockwise
+  		int offset = 0;
+  		for (int i = 0; i < 8; i++) {
+  			if (coordinates[i] == null) {
+  				offset = (i+1)%8; //mod 8 means it is the first element
+  			}
   		}
 
+  	
+  		if (offset !=0) {
+  		// go clockwise to reach the initial point if offset is not 0
+  	  		for (int i = 7; i >= offset; i--) {
+  	  			nav.syncTravelTo(coordinates[i][0], coordinates[i][1]);
+  	  		}
+  		}
+  		
+  		// realCoordinates represents the list of actual coordinates to go to
+  		// keep track of where it ends relative to the start
+  		int endOffset = 0;
+  		for (int i = 0; i < 8; i++) {
+  			if (i %2 == 0) {
+  	  			nav.turnTo(odo.getXYT()[2], angles[((i+offset)%8)/2]);
+  	  			getRings();
+  			}
+  			if (coordinates[(i+offset+1)%8] != null) {
+  				nav.syncTravelTo(coordinates[(i+offset+1)%8][0], coordinates[(i+offset+1)%8][1]);
+  			} else {
+  				endOffset = (i+offset)%8;
+  				break;
+  			}
+  		}
+  		
+  		// go back to the original place so we can travel back using 
+  		// the exact same algorithm used to navigate here
+  		
+  		if (endOffset != 0) {
+  			for (int i = endOffset -1; i> 0; i--) {
+  				nav.syncTravelTo(coordinates[i][0], coordinates[i][1]);
+  			}
+  		}
+	}
+	
+	public static boolean isIn(float[][] area, float[] point) {
+		boolean overlapX = area[1][0] - area[0][0] >= point[0] - area[0][0];
+		boolean overlapY = area[1][1] - area[0][1] >= point[1] - area[0][1];
+		
+		return overlapX && overlapY;
 	}
 
 	/*
